4af9db07f98a76f2e4d1f1232bb6dfb6ae42956b
psynewave <mark@pezops.com>
2015-07-26 15:12:09 -0700

    Initial Flux implementation

--- app/assets/frontend/components/ResourceList.jsx
+++ app/assets/frontend/components/ResourceList.jsx
@@ -1,21 +1,28 @@
 import OneResource from "./OneResource"
-import Resource from "../stores/ResourceStore"
+import ResourceStore from "../stores/ResourceStore"
+
+let getStateFromStore = () => {
+  return { resources: ResourceStore.getAll() };
+}
 
 export default class ResourceList extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      resources: []
-    };
-
+    this.state = getStateFromStore();
+    this.onStoreChange = this.onStoreChange.bind(this);
   }
-  componentDidMount () {
-    // this.setState({
-    //   resources: Resource.fetchAll()
-    // });
+  onStoreChange() {
+    console.log("5. The store has changed update the state");
+    this.setState(getStateFromStore());
+  }
+  componentDidMount() {
+    ResourceStore.addChangeListener(this.onStoreChange);
+  }
+  componentWillUnmount() {
+    ResourceStore.removeChangeListener(this.onStoreChange);
   }
   render() {
+
     let resources = this.state.resources.map((resource, i) => {
       return <OneResource resource={resource} key={i} />;
     });
--- app/assets/frontend/stores/ResourceStore.jsx
+++ app/assets/frontend/stores/ResourceStore.jsx
@@ -1,7 +1,39 @@
+import AppDispatcher from "../dispatcher";
+import Actions from "../constants";
+
 let _resources = [];
 
-export default {
+import { EventEmitter } from "events";
+
+class ResourceEventEmitter extends EventEmitter {
   getAll() {
     return _resources;
   }
+  emitChange () {
+    this.emit("CHANGE");
+    console.log("4. Change event emitted");
+  }
+  addChangeListener(callback) {
+    this.on("CHANGE", callback);
+  }
+  removeChangeListener(callback) {
+    this.removeListener("CHANGE", callback);
+  }
 }
+
+let ResourseStore = new ResourceEventEmitter();
+
+AppDispatcher.register( action => {
+  switch (action.actionType) {
+    case Actions.RECEIVE_RESOURCES:
+      //do something
+      _resources = action.articles;
+      console.log('3 inside callback registered by the store', action.articles);
+      ResourseStore.emitChange();
+      break;
+    default:
+      //no operations
+  }
+});
+
+export default ResourseStore
